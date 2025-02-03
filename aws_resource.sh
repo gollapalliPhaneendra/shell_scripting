b2c741cd7e38ac312e92ddebc07f3443365ffa41
gollapalliphaneendra <gollapalli.phani@gmail.com>
2025-02-03 21:02:37 +0530

    inital commit

---
+++ aws_resource.sh
@@ -0,0 +1,14 @@
+#!bin/bash
+###################################
+#Shell Script to list the AWS resources in a Account 
+#Date:
+#Author: Phani
+#Version:v.0.1
+####################################
+
+#Check AWS CLI  is Configured or not
+if [[-z "$ (aws configuration is done or not)"]];then
+    echo "Error;
+fi
+
+
